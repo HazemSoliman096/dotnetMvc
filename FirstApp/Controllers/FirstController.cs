6fe2d40aa6a4ab72ba711a7387e7cc9aba2075fb
HazemSoliman096 <hazemsoliman096@gmail.com>
2023-01-03 09:32:22 +0200

    format

--- FirstApp/Controllers/FirstController.cs
+++ FirstApp/Controllers/FirstController.cs
@@ -1,23 +1,28 @@
 using FirstApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FirstApp.Controllers {
-  public class FirstController : Controller {
-    public string Index() {
-      return "Hello, World!";
-    }
+namespace FirstApp.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, World!";
+        }
 
-    public IActionResult Hello() {
-      ViewBag.Message = "Hello Message";
-      return View();
-    }
+        public IActionResult Hello()
+        {
+            ViewBag.Message = "Hello Message";
+            return View();
+        }
 
-    public IActionResult Info() {
-      Person person = new Person();
-      person.name = "Hazem";
-      person.Age = 26;
-      person.Location = "Egypt/Alexandria";
-      return View(person);
+        public IActionResult Info()
+        {
+            Person person = new Person();
+            person.name = "Hazem";
+            person.Age = 26;
+            person.Location = "Egypt/Alexandria";
+            return View(person);
+        }
     }
-  }
 }
