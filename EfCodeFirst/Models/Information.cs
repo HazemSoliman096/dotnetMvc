06bfe05e7371d302923a828b34b56968b5745613
HazemSoliman096 <hazemsoliman096@gmail.com>
2023-01-10 14:46:31 +0200

    add information model

---
+++ EfCodeFirst/Models/Information.cs
@@ -0,0 +1,15 @@
+namespace EfCodeFirst.Models
+{
+  public class Information {
+
+    public int Id { get; set; }
+
+    Public string Name { get; set; } = string.Empty;
+
+    public string License { get; set; } = string.Empty;
+
+    public DateTime Established { get; set; }
+
+    public decimal Revenue { get; set; }
+  }
+}
