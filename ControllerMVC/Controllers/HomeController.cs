470d9f2db2c463650c9dc599e06d15fdb3efb683
HazemSoliman096 <hazemsoliman096@gmail.com>
2023-01-15 21:41:43 +0200

    Request Data By Request.

--- ControllerMVC/Controllers/HomeController.cs
+++ ControllerMVC/Controllers/HomeController.cs
@@ -1,31 +1,16 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using ControllerMVC.Models;
-
+﻿using Microsoft.AspNetCore.Mvc;
 namespace ControllerMVC.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  public IActionResult Index()
+  {
+    return View();
+  }
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  public IActionResult ReceivedDataByRequest() {
+    string? name = Request.Form["name"];
+    string? sex = Request.Form["sex"];
+    return View("ReceivedDataByRequest", $"{name} sex is {sex}");
+  }
 }
